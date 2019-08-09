ce585f1413d81f2ca75bf60a1f8ad627bfead781
ChinaLike <572919350@qq.com>
2019-08-09 14:26:30 +0800

    1.测试代码

---
+++ __tests__/index.js
@@ -0,0 +1,51 @@
+import { format2Dot, totalPrice } from '../app/utils/index'
+
+describe('format2Dot tests', () => {
+  it('should add zero for int numbers and return them as string', () => {
+    expect(format2Dot(0)).toEqual('0.00')
+    expect(format2Dot(1)).toEqual('1.00')
+    expect(format2Dot(2)).toEqual('2.00')
+    expect(format2Dot(3)).toEqual('3.00')
+    expect(format2Dot(4)).toEqual('4.00')
+    expect(format2Dot(5)).toEqual('5.00')
+    expect(format2Dot(6)).toEqual('6.00')
+    expect(format2Dot(7)).toEqual('7.00')
+    expect(format2Dot(8)).toEqual('8.00')
+    expect(format2Dot(9)).toEqual('9.00')
+  })
+
+  it('should add zero for float numbers and return them as string', () => {
+    expect(format2Dot(1.1)).toEqual('1.10')
+    expect(format2Dot(2.2)).toEqual('2.20')
+  })
+
+  it('should add remain 2 decimal places for float numbers and return them as string', () => {
+    expect(format2Dot(1.11111)).toEqual('1.11')
+    expect(format2Dot(2.2123123123)).toEqual('2.21')
+    expect(format2Dot(2.00000222)).toEqual('2.00')
+  })
+
+  it('should return 0.00 when not number and return them as string', () => {
+    expect(format2Dot('aaa')).toEqual('0.00')
+    expect(format2Dot('$@#@#')).toEqual('0.00')
+    expect(format2Dot('$@#@#123')).toEqual('0.00')
+  })
+})
+
+describe('totalPrice tests', () => {
+  it('should add zero for int numbers and return them as string', () => {
+    expect(totalPrice([{ price: 190.5, amount: 6 }])).toEqual(format2Dot(190.5 * 6))
+    expect(totalPrice([{ price: 1, amount: 6 }])).toEqual(format2Dot(6))
+    expect(totalPrice([{}])).toEqual(format2Dot(0))
+  })
+
+  it('mutiple product and return them as string', () => {
+    expect(totalPrice([{ price: 190.5, amount: 6 }, { price: 1, amount: 6 }])).toEqual(
+      format2Dot(190.5 * 6 + 1 * 6)
+    )
+    expect(totalPrice([{ price: 1, amount: 6 }, { price: 2.001, amount: 6 }])).toEqual(
+      format2Dot(6 + 2.001 * 6)
+    )
+    expect(totalPrice([{ price: 1, amount: 6 }, {}])).toEqual(format2Dot(6))
+  })
+})
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash'
+
 export { NavigationActions, StackActions } from 'react-navigation'
 
 export { default as Storage } from './storage'
@@ -5,3 +7,27 @@ export { default as Storage } from './storage'
 export const delay = time => new Promise(resolve => setTimeout(resolve, time))
 
 export const createAction = type => payload => ({ type, payload })
+
+export const format2Dot = number => {
+  const floatNumber = parseFloat(number)
+  if (_.isNaN(floatNumber)) {
+    return '0.00'
+  }
+
+  return `${floatNumber.toFixed(2)}`
+}
+
+export const totalPrice = products => {
+  const price = _.reduce(
+    products,
+    (sum, product) => sum + (product?.price ?? 0) * (product?.amount ?? 0),
+    0
+  )
+
+  const floatNumber = parseFloat(price)
+  if (_.isNaN(floatNumber)) {
+    return '0.00'
+  }
+
+  return `${floatNumber.toFixed(2)}`
+}
